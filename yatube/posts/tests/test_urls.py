f495990eda81d93d54136c94db4b24403b50770a
Василий <Veisner@mail.ru>
2022-06-10 13:10:58 +0300

    rev5

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from django.test import Client, TestCase
+from django.urls import reverse
 
 from ..models import Group, Post, User
 
@@ -28,29 +29,41 @@ class ViewsURLTests(TestCase):
         self.author_client.force_login(ViewsURLTests.user_author)
         self.not_author_client = Client()
         self.not_author_client.force_login(ViewsURLTests.user_not_author)
-        self.guest_url_templates = {
-            '/': 'posts/index.html',
-            '/group/test_slug/': 'posts/group_list.html',
-            '/profile/author/': 'posts/profile.html',
-            '/posts/1/': 'posts/post_detail.html',
-        }
         self.auth_url_templates = {
             '/posts/1/edit/': 'posts/post_create.html',
             '/create/': 'posts/post_create.html',
         }
-        self.url_templates = (self.guest_url_templates
-                              | self.auth_url_templates)
+        self.templates_page_names = {
+            reverse('posts:index'): 'posts/index.html',
+            reverse('posts:group_list', kwargs={'slug': 'test_slug'}
+                    ): 'posts/group_list.html',
+            reverse('posts:profile', kwargs={
+                    'username': ViewsURLTests.user_author}
+                    ): 'posts/profile.html',
+            reverse('posts:post_detail', kwargs={'post_id': 1}
+                    ): 'posts/post_detail.html',
+            reverse('posts:post_edit', kwargs={'post_id': 1}
+                    ): 'posts/post_create.html',
+            reverse('posts:post_create'): 'posts/post_create.html',
+        }
+
+    def test_pages_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        for reverse_name, template in self.templates_page_names.items():
+            with self.subTest(template=template):
+                response = self.author_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
 
     def test_urls(self):
         """Страницы доступны любому пользователю"""
-        for url in self.guest_url_templates:
+        for url in self.templates_page_names:
             with self.subTest(url=url):
                 response = self.guest_client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_urls(self):
         """Страницы доступны авторизованному пользователю"""
-        for url in self.auth_url_templates:
+        for url in self.templates_page_names:
             with self.subTest(url=url):
                 response = self.author_client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
@@ -59,17 +72,8 @@ class ViewsURLTests(TestCase):
         """Страницы create и post_edit перенаправят анонимного пользователя"""
         for url in self.auth_url_templates:
             with self.subTest(url=url):
-                response = self.guest_client.get(url)
+                response = self.guest_client.post(url)
                 self.assertEqual(response.status_code, HTTPStatus.FOUND)
-
-    def test_urls_uses_correct_template(self):
-        """URL-адрес использует соответствующий шаблон."""
-        for url, template in self.url_templates.items():
-            with self.subTest(url=url):
-                response = self.author_client.get(url)
-                self.assertTemplateUsed(
-                    response, template,
-                )
 
     def test_unexisting_page_url_exists_at_desired_location(self):
         """Страница /unexisting_page/ вернет кастомную ошибку 404."""
